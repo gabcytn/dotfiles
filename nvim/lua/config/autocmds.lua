fd05b5f03ec25ae604f6ff7d7fa363b19d33774f
gabcytn <cayetanogabriel03@gmail.com>
2026-06-09 21:50:43 +0800

    refactor(nvim): change colorscheme to gruvbox

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,3 +6,7 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.cmd.colorscheme("gruvbox")
+vim.cmd("hi! Normal guibg=None ctermbg=None")
+vim.cmd("hi! NonText guibg=None ctermbg=None")
